ac7344c5a5d0f6f1c18ed7eb972330108164a757
Diego-Armando-MTZ <diegoarmandomartinez@ciencias.unam.mx>
2026-05-20 15:52:11 +0000

    creacion de arquitectura MVC

---
+++ Sistema_empleados/src/mx/edu/unam/sistemaempleados/model/empleado.java
@@ -0,0 +1,12 @@
+package mx.edu.unam.sistemaempleados.model;
+import java.io.Serializable;
+public class empleado implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    protected String nombre;
+    protected String id;
+    protected String salario;
+    protected String areapuesto;
+
+
+}
